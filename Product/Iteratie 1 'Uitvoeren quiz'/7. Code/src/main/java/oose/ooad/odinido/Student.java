e8b0ca61e03c4a256d0355cfc8786a10de81d42c
KlaasvdLinden <36151784+KlaasvdLinden@users.noreply.github.com>
2018-10-22 11:57:17 +0200

    Uitvoerkennnistoets onnodige methods weggehaald

--- Product/Iteratie 1 'Uitvoeren quiz'/7. Code/src/main/java/oose/ooad/odinido/Student.java
+++ Product/Iteratie 1 'Uitvoeren quiz'/7. Code/src/main/java/oose/ooad/odinido/Student.java
@@ -5,30 +5,21 @@ import java.util.Scanner;
 public class Student {
 
     private String studentnaam;
-    private boolean studentBezig = false;
 
     public Student(String studentnaam) {
         this.studentnaam = studentnaam;
     }
 
-    public void beginKennisToets() {
-        studentBezig = true;
-    }
+
 
     public void uitvoerenKennistoets(Kennistoets kennistoets) {
         Scanner scanner = new Scanner(System.in);
-        beginKennisToets();
-        while (studentBezig) {
             for (Vraag v : kennistoets.getVragen()) {
                 String vraagData = kennistoets.getVraagData(v);
                 System.out.println(vraagData);
                 String antwoord = scanner.nextLine();
                 kennistoets.setAntwoord(antwoord, v);
-                if (kennistoets.getVragen().indexOf(v) == (kennistoets.getVragen().size() - 1)) {
-                    studentBezig = false;
-                }
             }
-        }
         System.out.println("Klaar met toets");
         for (Vraag vraag : kennistoets.getVragen()) {
             Vraag v = vraag;
@@ -36,6 +27,4 @@ public class Student {
             System.out.println("TekstAntwoord correct ? " + v.isCorrect());
         }
     }
-
-
 }
